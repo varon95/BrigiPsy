7133e29e28ad2cdeaba06406a0e53199de0a4a3e
Áron Váradi <ron Vradi@DESKTOP-L2I9MC1>
2024-11-03 15:21:44 +0100

    Add project files.

---
+++ BrigiPsy/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BrigiPsy.Models;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace BrigiPsy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ContactFormModel());
+        }
+
+        public IActionResult DataUsage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("BrigiPsy Website", "no-reply@brigipsy.hu"));
+                message.To.Add(new MailboxAddress("Brigitta", "borbasbrege@gmail.com"));
+                message.Subject = $"időpontkérés - {model.Name}";
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = $"Név: {model.Name}\nEmail: {model.Email}\nÜzenet:\n{model.Message}"
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                    await client.ConnectAsync("smtp.gmail.com", 587, false);
+                    await client.AuthenticateAsync("borbasbrege@gmail.com", "kJkcal8966091");
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+
+                TempData["Success"] = "Üzenetét sikeresen elküldtük.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ BrigiPsy/Models/ContactFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrigiPsy.Models
+{
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = "A név megadása kötelező.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Az email megadása kötelező.")]
+        [EmailAddress(ErrorMessage = "Érvényes email címet adjon meg.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Az üzenet megadása kötelező.")]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "Az adatkezelési nyilatkozat elfogadása kötelező.")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Az adatkezelési nyilatkozat elfogadása kötelező.")]
+        public bool AcceptPrivacyPolicy { get; set; }
+    }
+}
